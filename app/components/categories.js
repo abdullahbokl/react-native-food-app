1bec0e0675bd01649111714361e95968a4003526
Bokl2002 <abdullah.bokl@gmail.com>
2024-01-25 05:20:24 +0200

    feature/ categories list finished

--- app/components/categories.js
+++ app/components/categories.js
@@ -1,33 +1,10 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import React, { useState, useEffect } from "react";
-import AppColors from "../theme/appColors";
-
-const itemsList = [
-  {
-    name: "Pizza",
-    image: "https://source.unsplash.com/featured/?pizza",
-  },
-  {
-    name: "Burger",
-    image: "https://source.unsplash.com/featured/?burger",
-  },
-  {
-    name: "Sushi",
-    image: "https://source.unsplash.com/featured/?sushi",
-  },
-  {
-    name: "Desserts",
-    image: "https://source.unsplash.com/featured/?desserts",
-  },
-  {
-    name: "Drinks",
-    image: "https://source.unsplash.com/featured/?drinks",
-  },
-];
+import categoriesData from "../constants/shared/categoriesData";
 
 export default function Categories() {
   const [activeCategory, setActiveCategory] = useState(null);
-  const [categories, setCategories] = useState(itemsList);
+  const [categories, setCategories] = useState(categoriesData);
   useEffect(() => {}, []);
 
   return (
@@ -41,21 +18,36 @@ export default function Categories() {
         }}
       >
         {categories?.map((category) => {
-          let isActive = category._id == activeCategory;
+          let isActive = category.id == activeCategory;
           let btnClass = isActive ? " bg-gray-600" : " bg-gray-200";
           let textClass = isActive
-            ? " font-semibold text-gray-800"
-            : " text-gray-500";
+            ? "font-semibold text-gray-800"
+            : "text-gray-500";
           return (
             <View
-              key={category._id}
-              className="flex justify-center items-center mr-6"
+              key={category.id}
+              className="flex justify-center items-center mr-5"
             >
               <TouchableOpacity
-                onPress={() => setActiveCategory(category._id)}
-                className={"p-1 rounded-full shadow" + btnClass}
+                onPress={() => setActiveCategory(category.id)}
+                className={"p-1 rounded-full shadow " + btnClass}
               >
-                <Image style={{ width: 45, height: 45 }} source={{}} />
+                <Image
+                  style={{
+                    width: 45,
+                    height: 45,
+                    borderRadius: 60,
+                  }}
+                  source={
+                    category.image
+                      ? { uri: category.image }
+                      : {
+                          uri:
+                            "https://source.unsplash.com/featured/?food" +
+                            category.name,
+                        }
+                  }
+                />
               </TouchableOpacity>
               <Text className={"text-sm " + textClass}>{category.name}</Text>
             </View>
---
+++ app/constants/shared/categoriesData.js
@@ -0,0 +1,124 @@
+const categoriesData = [
+  {
+    id: 0,
+    name: "Burgers",
+    image: "https://source.unsplash.com/featured/?burgers",
+  },
+  {
+    id: 1,
+    name: "Chicken",
+    image: "https://source.unsplash.com/featured/?chicken",
+  },
+  {
+    id: 2,
+    name: "Desserts",
+    image: "https://source.unsplash.com/featured/?desserts",
+  },
+  {
+    id: 3,
+    name: "Duck",
+    image: "https://source.unsplash.com/featured/?duck",
+  },
+  {
+    id: 4,
+    name: "Drinks",
+    image: "https://source.unsplash.com/featured/?drinks",
+  },
+  {
+    id: 5,
+    name: "Lamb",
+    image: "https://source.unsplash.com/featured/?lamb",
+  },
+  {
+    id: 6,
+    name: "Meat",
+    image: "https://source.unsplash.com/featured/?meat",
+  },
+  {
+    id: 7,
+    name: "Mince",
+    image: "https://source.unsplash.com/featured/?mince",
+  },
+  {
+    id: 8,
+    name: "Noodles",
+    image: "https://source.unsplash.com/featured/?noodles",
+  },
+  {
+    id: 9,
+    name: "Pasta",
+    image: "https://source.unsplash.com/featured/?pasta",
+  },
+  {
+    id: 10,
+    name: "Pies",
+    image: "https://source.unsplash.com/featured/?pies",
+  },
+  {
+    id: 11,
+    name: "Pizza",
+    image: "https://source.unsplash.com/featured/?pizza",
+  },
+  {
+    id: 12,
+    name: "Pork",
+    image: "https://source.unsplash.com/featured/?pork",
+  },
+  {
+    id: 13,
+    name: "Poultry",
+    image: "https://source.unsplash.com/featured/?poultry",
+  },
+  {
+    id: 14,
+    name: "Rice",
+    image: "https://source.unsplash.com/featured/?rice",
+  },
+  {
+    id: 15,
+    name: "Salads",
+    image: "https://source.unsplash.com/featured/?salads",
+  },
+  {
+    id: 16,
+    name: "Sauces",
+    image: "https://source.unsplash.com/featured/?sauces",
+  },
+  {
+    id: 17,
+    name: "Seafood",
+    image: "https://source.unsplash.com/featured/?seafood",
+  },
+  {
+    id: 18,
+    name: "Sides",
+    image: "https://source.unsplash.com/featured/?sides",
+  },
+  {
+    id: 19,
+    name: "Snacks",
+    image: "https://source.unsplash.com/featured/?snacks",
+  },
+  {
+    id: 20,
+    name: "Soups",
+    image: "https://source.unsplash.com/featured/?soups",
+  },
+  {
+    id: 21,
+    name: "Stir Fry",
+    image: "https://source.unsplash.com/featured/?stirFry",
+  },
+  {
+    id: 22,
+    name: "Turkey",
+    image: "https://source.unsplash.com/featured/?turkey",
+  },
+  {
+    id: 23,
+    name: "Vegetarian",
+    image: "https://source.unsplash.com/featured/?vegetarian",
+  },
+];
+
+export default categoriesData;
